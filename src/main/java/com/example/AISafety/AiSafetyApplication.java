5bd7ed9e0343569bda00918f14413af58437a74b
park_yeongyu <sjjs7771@naver.com>
2025-01-13 11:30:25 +0900

    feat: 초기세팅

---
+++ src/main/java/com/example/AISafety/AiSafetyApplication.java
@@ -0,0 +1,13 @@
+package com.example.AISafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSafetyApplication.class, args);
+	}
+
+}
