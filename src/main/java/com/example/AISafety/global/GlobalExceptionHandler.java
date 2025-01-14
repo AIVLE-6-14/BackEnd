2881ff8ae3b83312fe9038da0d97949f009bd745
park_yeongyu <sjjs7771@naver.com>
2025-01-14 21:41:14 +0900

    feat : swaggerUI 추가

---
+++ src/main/java/com/example/AISafety/global/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.AISafety.global;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@Hidden
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //IllegalArgumentException 예외 처리
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String,String>> handleIllegalArgumentException(IllegalArgumentException ex){
+        Map<String, String> response = new HashMap<>();
+        response.put("ERROR", "error");
+        response.put("Message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
