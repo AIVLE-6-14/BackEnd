5edf1d6e43ea914917448b63e429fd9915f0f56e
park_yeongyu <sjjs7771@naver.com>
2025-01-15 01:09:38 +0900

    feat : followup 최초 등록 로직 구현 1. 자체 처리 : organization_id 을 1로 status를 Pending 상태로 animal_id 를 인자로 받은 animal로 하여 최초등록 2. 타 기관 요청 처리: organization_id, animal_id를 인자로 받고 status를 Pending 상태로 두어 최초등록 3.user_id NOT NULL 조건 제거

--- src/main/java/com/example/AISafety/domain/followup/repository/FollowUpRepository.java
+++ src/main/java/com/example/AISafety/domain/followup/repository/FollowUpRepository.java
@@ -1,10 +1,14 @@
 package com.example.AISafety.domain.followup.repository;
 
 import com.example.AISafety.domain.followup.FollowUp;
+import com.example.AISafety.domain.followup.Status;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FollowUpRepository extends JpaRepository<FollowUp, Long> {
+    List<FollowUp> findByOrganizationIdAndStatus(Long organizationId, Status status);
 
+    List<FollowUp> findByStatus(Status status);
 }
---
+++ src/main/java/com/example/AISafety/domain/followup/service/FollowUpService.java
@@ -0,0 +1,55 @@
+package com.example.AISafety.domain.followup.service;
+
+import com.example.AISafety.domain.animal.Animal;
+import com.example.AISafety.domain.followup.FollowUp;
+import com.example.AISafety.domain.followup.Status;
+import com.example.AISafety.domain.followup.repository.FollowUpRepository;
+import com.example.AISafety.domain.organization.service.OrganizationService;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FollowUpService {
+    private final FollowUpRepository followUpRepository;
+    private final OrganizationService organizationService;
+
+    public Map<String,String> selfHandle(Animal animal){
+
+        FollowUp follow = new FollowUp();
+        follow.setAnimal(animal);
+        follow.setOrganization(organizationService.getOrganizationByID(1L));
+        follow.setStatus(Status.PENDING);
+        follow.setCreatedAt(LocalDateTime.now());
+
+        followUpRepository.save(follow);
+        Map<String, String> response = new HashMap<>();
+        response.put("success", "follow 저장 성공!");
+        return response;
+    }
+
+    public Map<String, String> otherHandle(Animal animal, Long organizationId){
+        FollowUp follow = new FollowUp();
+        follow.setAnimal(animal);
+        follow.setOrganization(organizationService.getOrganizationByID(organizationId));
+        follow.setStatus(Status.PENDING);
+        follow.setCreatedAt(LocalDateTime.now());
+
+        followUpRepository.save(follow);
+        Map<String, String> response = new HashMap<>();
+        response.put("success", "follow 저장 성공!");
+        return response;
+    }
+
+    public List<FollowUp> findByOrganizationIdAndStatus(Long id, Status status) {
+        return followUpRepository.findByOrganizationIdAndStatus(id, status);
+    }
+
+    public List<FollowUp> findByStatus(Status status){
+        return followUpRepository.findByStatus(status);
+    }
+}
