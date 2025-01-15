8997126ad0f629d9fce5fea0b21a2496e92490c0
park_yeongyu <sjjs7771@naver.com>
2025-01-15 14:03:06 +0900

    Swagger 설명 추가

--- src/main/java/com/example/AISafety/domain/post/controller/PostController.java
+++ src/main/java/com/example/AISafety/domain/post/controller/PostController.java
@@ -3,6 +3,8 @@ package com.example.AISafety.domain.post.controller;
 import com.example.AISafety.domain.post.dto.PostRequestDTO;
 import com.example.AISafety.domain.post.dto.PostResponseDTO;
 import com.example.AISafety.domain.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
@@ -19,11 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
+@Tag(name="Post API", description = "게시물 관련 API 제공합니다.")
 public class PostController {
 
     private final PostService postService;
 
     @PostMapping("/create")
+    @Operation(summary="게시물 생성 기능", description = "동물id, 제목, 내용, 유저 세션을 받아서 게시물을 생성합니다.")
     public ResponseEntity<Map<String,String>> createPost(@RequestBody PostRequestDTO requestDTO, HttpSession session) {
         Map<String, String> response = new HashMap<>();
 
@@ -46,6 +50,7 @@ public class PostController {
 
         // 모든 포스트 조회
         @GetMapping("/all")
+        @Operation(summary="게시물 전체 조회 기능", description = "모든 게시물을 보여줍니다.")
         public ResponseEntity<List<PostResponseDTO>> getAllPosts(){
             List<PostResponseDTO> allPosts = postService.getAllPosts();
             return ResponseEntity.ok(allPosts);
@@ -53,6 +58,7 @@ public class PostController {
 
         // 특정 기관의 포스트 조회
         @GetMapping("/organization")
+        @Operation(summary="게시물 기관 조회 기능", description = "해당 유저의 세션을 받아 해당 유저가 속한 기관의 게시물을 보여줍니다.")
         public ResponseEntity<List<PostResponseDTO>> getPostsByOrganization(HttpSession session){
             List<PostResponseDTO> postsByOrganization = postService.getPostsByOrganization(session);
             return ResponseEntity.ok(postsByOrganization);
