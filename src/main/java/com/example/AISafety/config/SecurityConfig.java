dd112dfb05926b583ceeaba389bc0f72e6d3f5fa
park_yeongyu <sjjs7771@naver.com>
2025-01-20 12:28:30 +0900

    feat : 비밀번호 암호화, h2, swagger 인증 해제

--- src/main/java/com/example/AISafety/config/SecurityConfig.java
+++ src/main/java/com/example/AISafety/config/SecurityConfig.java
@@ -1,28 +1,46 @@
 package com.example.AISafety.config;
 
+import com.example.AISafety.global.security.jwt.JwtAuthenticationFilter;
+import com.example.AISafety.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 비활성화
                 .and()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/login", "/api/users/signup","/api/users/logout","/api/animals/fetch").permitAll()
+                        .requestMatchers("/","/api/users/login", "/api/users/signup","/api/users/logout","/api/animals/fetch","api/organizations/save","api/animals/1/self-handle").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(
+                                "swagger-ui/**", //swagger UI 접근 허용
+                                "/v3/api-docs/**", //swagger OpenAPI 문서
+                                "/swagger-resources/**", // swagger 리소스
+                                "/webjars/**" // swagger webjar
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
