ea1ac700988e5a00f5670e461fabf209cfd77143
park_yeongyu <sjjs7771@naver.com>
2025-02-04 16:37:02 +0900

    fix : LLM 사용을 위해 기본 정보 넘겨주는 로직 작성

--- src/main/java/com/example/AISafety/domain/post/dto/InfoResponseDTO.java
+++ src/main/java/com/example/AISafety/domain/post/dto/InfoResponseDTO.java
@@ -14,6 +14,9 @@ import lombok.Setter;
 public class InfoResponseDTO {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d9774d4 (fix : LLM 사용을 위해 기본 정보 넘겨주는 로직 작성)
 
     @JsonProperty("detected_time")
     private LocalDateTime detectedTime;
@@ -22,10 +25,13 @@ public class InfoResponseDTO {
     @JsonProperty("incident_time")
     private LocalDateTime incidentTime;
 >>>>>>> f1d5c5b (fix : LLM 사용을 위해 기본 정보 넘겨주는 로직 작성)
+<<<<<<< HEAD
 =======
     @JsonProperty("detected_time")
     private LocalDateTime detectedTime;
 >>>>>>> 140ef00 (fix : dto 변경)
+=======
+>>>>>>> d9774d4 (fix : LLM 사용을 위해 기본 정보 넘겨주는 로직 작성)
 
     private double latitude;
 
